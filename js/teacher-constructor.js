871b56cbf9f19db447d4ec7adaa032ca55062dcc
Kobe Simon <kobesimon@wl.k12.ia.us>
2018-11-16 13:21:44 -0600

    Teachers display

--- js/teacher-constructor.js
+++ js/teacher-constructor.js
@@ -1,22 +1,25 @@
 'use strict';
 
-var REPLACE1 = [];
+var teacher_list = [];
 
-var REPLACE2 = function(teacher){
-    
+var Teacher = function(teacher){
+    this.image = teacher.image;
+    this.name= teacher.name;
+    this.subjects = teacher.subjects;
+    this.teacher_id = teacher.teacher_id;
 };
 
-REPLACE2.prototype.toHtml = function(){
+Teacher.prototype.toHtml = function(){
     var source = $('#teacher-template').text();
     var template = Handlebars.compile(source);
     return template(this);
 };
 
-teachers.forEach(function(REPLACE3){
-    REPLACE1.push(new REPLACE2(REPLACE3));
+teachers.forEach(function(teacher){
+    teacher_list.push(new Teacher(teacher));
 });
 
 
-REPLACE1.forEach(function(REPLACE3){
-    $('#teacher-listings').append(REPLACE3.toHtml());
+teacher_list.forEach(function(teacher){
+    $('#teacher-listings').append(teacher.toHtml());
 });
---
+++ js/teachers.js
@@ -0,0 +1,32 @@
+var teachers = [
+    {
+        image:"jennings.jpg",
+        name:"Ms. Jennings",
+        subjects:["English", "College Writing"],
+        teacher_id:"C1000"
+    },
+     {
+        image:"hood.jpg",
+        name:"Ms. Hood",
+        subjects:["Textile", "Beginning foods", "Child development"],
+        teacher_id:"C1001"
+    },
+    {
+        image:"harvey.jpg",
+        name:"Mrs. Harvey",
+        subjects:["Ap Calculus", "Math 3", "Ap Stats" ],
+        teacher_id:"C1002"
+    },
+    {
+        image:"diemer.jpg",
+        name:"Mr. Diemer",
+        subjects:["Modern U.S History", "Sociology",],
+        teacher_id:"C1003"
+    },
+    {
+        image:"hauschildt.jpg",
+        name:"Mr. Hauschildt",
+        subjects:["Marketing", "Ap Computer Science",  ],
+        teacher_id:"C1004"
+    },
+];
